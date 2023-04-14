d564a8e300b0772d948418f470a79b45c64d839d
soyekwon <soye0710@naver.com>
2023-04-14 20:30:09 +0900

    feat: Card dummyData 넘겨주기

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,18 @@
 import { Flex, Box, Label } from 'components/Box';
+import { useDate } from 'hooks/useDate';
 import PersonIcon from '@mui/icons-material/Person';
+import cardType from './types';
 
-const Card = () => {
+interface Props {
+  card: cardType;
+}
+
+const Card = (props: Props) => {
+  const { card } = props;
   return (
     <>
       <Box width="15rem" height="10rem" borderRadius="1.25rem" background="#fff8d6" />
-      <Flex p={1} flexDirection="column" gridGap={1}>
+      <Flex p={2} flexDirection="column" gridGap={1}>
         <Flex>
           <Label
             height="1.2rem"
@@ -16,33 +23,37 @@ const Card = () => {
             p={1}
             mt={1}
           >
-            진행중
+            {card.status}
           </Label>
-          <Label fontSize="0.7rem" color="#888888" m={2} mr={4}>
-            2023.04.01 ~ 2023.06.23
+          <Label fontSize="0.7rem" color="#888888" m={2} mr={3}>
+            {useDate(new Date())} ~ {useDate(new Date())}
           </Label>
           <Flex flexDirection="column">
             <PersonIcon sx={{ color: '#888888', fontSize: 20 }} />
             <Label fontSize="0.1px" color="#888888">
-              435
+              {card.cnt}
             </Label>
           </Flex>
         </Flex>
 
         <Label width="14.5rem" fontSize="1rem" fontWeight={800}>
-          알파프로젝트 알파프로젝트 알파프로젝트에 대해 알아보자
+          {card.title}
         </Label>
-        <Label
-          width="3rem"
-          height="1.1rem"
-          fontSize="0.1rem"
-          color="white"
-          borderRadius={10}
-          background="#949494"
-          p={1}
-        >
-          인구통계
-        </Label>
+        <Flex>
+          {card.categorys.map((category) => (
+            <Label
+              key={category}
+              fontSize="0.1rem"
+              color="white"
+              borderRadius={10}
+              background="#949494"
+              p={1}
+              mr={1}
+            >
+              {category}
+            </Label>
+          ))}
+        </Flex>
       </Flex>
     </>
   );
