8c50d452a6efcea311a5293d89062b5f217e9026
김지형 <jihyoung9912@gmail.com>
2023-04-16 03:21:33 +0900

    feat: logo 메인페이지 이동 로직 구현

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,74 @@
+import * as React from 'react';
 import styled from 'styled-components';
 import Flex from 'components/Box/Flex';
 
 const FooterBox = styled(Flex)`
   width: 100%;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 1rem 3rem;
+  margin-top: 1rem;
+  background-color: #e77853;
+  color: #fff;
+`;
+
+const FooterTop = styled(Flex)`
+  width: 100%;
   justify-content: space-between;
-  padding: 15px 20px;
-  box-shadow: 0 -7px 5px -2px rgba(0, 0, 0, 0.2);
-  margin-top: 10px;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 1rem;
+  border-bottom: 1px solid #fff;
+`;
+
+const FooterAddress = styled.div`
+  text-align: center;
+  font-size: 13px;
+`;
+
+const FooterInfo = styled(Flex)`
+  width: 13rem;
+  justify-content: space-between;
+`;
+
+const FooterTerms = styled.a`
+  font-size: 18px;
+  font-weight: 800;
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const FooterBottom = styled(Flex)`
+  width: 100%;
+  justify-content: center;
+  margin-top: 1rem;
+  font-size: 10px;
 `;
 
 const Footer = () => {
-  return <FooterBox>Footer</FooterBox>;
+  return (
+    <FooterBox>
+      <FooterTop>
+        <a href="/">
+          <img
+            src="/assets/images/logo-black.svg"
+            alt="IBELEVESURVEY Logo"
+            style={{ width: '7rem', marginRight: '2rem' }}
+          />
+        </a>
+        <FooterInfo>
+          <FooterTerms href="/">이용약관</FooterTerms>
+          <FooterTerms href="/">개인정보처리방침</FooterTerms>
+        </FooterInfo>
+        <FooterAddress>
+          아빌립서베이 | 국민대학교 | 서울특별시 성북구 정릉로 77 <br />
+          사업자번호: 327-69-00498
+        </FooterAddress>
+      </FooterTop>
+      <FooterBottom>copyright@IBelieveSurvey</FooterBottom>
+    </FooterBox>
+  );
 };
 
 export default Footer;
