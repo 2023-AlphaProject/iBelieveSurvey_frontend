45c24cdc3a01e4fd8464ee02107a11a5353bba4e
김지형 <jihyoung9912@gmail.com>
2023-04-16 19:15:47 +0900

    feat: notfound page 초안

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,14 +1,43 @@
+import { Flex } from 'components/Box';
 import styled from 'styled-components';
 
-const ErrorImg = styled.img`
+const ErrorContainer = styled(Flex)`
   width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const ErrorImg = styled.img`
+  margin-top: 10vh;
+  width: 50%;
+  height: 30%;
+`;
+
+const ErrorMsg = styled.div`
+  width: 100%;
+  margin-top: 3rem;
+  font-size: 3rem;
+  font-weight: 700;
+  text-align: center;
+  color: #808080;
+`;
+
+const ToHomeButton = styled.button`
+  width: 5rem;
+  height: 2rem;
+  background-color: red;
 `;
 
 const NotFound = () => {
   return (
-    <div className="mainbox">
-      <ErrorImg src="/assets/images/Error-404.png" alt="404 Not Found Error Imgae" />
-    </div>
+    <ErrorContainer>
+      <ErrorImg
+        src={`${process.env.REACT_APP_BASE_URL}/assets/images/Error-404.png`}
+        alt="404 Not Found Error Imgae"
+      />
+      <ErrorMsg>페이지를 찾을 수 없습니다.</ErrorMsg>
+      <ToHomeButton>홈으로 가기</ToHomeButton>
+    </ErrorContainer>
   );
 };
 
