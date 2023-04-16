6a1bac86779cfcdb42d5bf0fa18ee22d2a8d04b3
김지형 <jihyoung9912@gmail.com>
2023-04-16 15:56:07 +0900

    feat: 404 error img add

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,13 @@
+import styled from 'styled-components';
+
+const ErrorImg = styled.img`
+  width: 100%;
+`;
+
 const NotFound = () => {
   return (
     <div className="mainbox">
-      <div className="err">4</div>
-      <i className="far fa-question-circle fa-spin">0</i>
-      <div className="err2">4</div>
-      <div className="msg">
-        Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first
-        place?
-        <p>
-          Lets go <a href="/">home</a> and try from there.
-        </p>
-      </div>
+      <ErrorImg src="/assets/images/Error-404.png" alt="404 Not Found Error Imgae" />
     </div>
   );
 };
