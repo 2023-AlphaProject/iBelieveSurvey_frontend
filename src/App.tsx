1e26200c12eb28ea12760a766173d72bdc3229cf
minjj0905 <minjj0905@daum.net>
2023-04-19 22:29:20 +0900

    style: font 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import GlobalStyles from 'styles/GlobalStyles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from 'layouts/Layout';
 import { Home, Test, NotFound } from 'pages';
-import Modal from 'components/common/Modal/Modal';
+import { Modal } from 'components/common';
 import { RecoilRoot } from 'recoil';
 import { SnackbarProvider } from 'notistack';
+import 'styles/fonts.css';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
             <Route element={<Layout />}>
               <Route path="/" element={<Home />} />
               <Route path="/test" element={<Test />} />
+              <Route path="*" element={<NotFound />} />
             </Route>
           </Routes>
         </Router>
