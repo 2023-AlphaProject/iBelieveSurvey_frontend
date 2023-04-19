108d416c7af7c76ff87ee1c2023633e336c0744f
minjj0905 <minjj0905@daum.net>
2023-04-19 21:25:09 +0900

    fix: change image path

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -89,7 +89,7 @@ const Navbar = () => {
             {/* PC Logo */}
             <Link to="/">
               <img
-                src={`${process.env.REACT_APP_BASE_URL}/assets/images/logo-black.svg`}
+                src={`${process.env.PUBLIC_URL}/assets/images/logo-black.svg`}
                 alt="IBELEVESURVEY Logo"
                 style={{ width: '7rem', marginRight: '2rem' }}
               />
@@ -159,7 +159,7 @@ const Navbar = () => {
             {/* Mobile Logo */}
             <Link to="/">
               <img
-                src={`${process.env.REACT_APP_BASE_URL}/assets/images/logo-black.svg`}
+                src={`${process.env.PUBLIC_URL}/assets/images/logo-black.svg`}
                 alt="IBELEVESURVEY Logo"
                 style={{ width: '5rem' }}
               />
