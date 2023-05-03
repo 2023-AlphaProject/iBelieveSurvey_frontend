81f2a38230391a52358876626696ea38fa3ab465
minjj0905 <minjj0905@daum.net>
2023-05-03 21:06:49 +0900

    fix: storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,27 @@
+import { BrowserRouter } from 'react-router-dom';
+import { theme, GlobalStyles } from 'styles';
+import { ThemeProvider } from 'styled-components';
+import { SnackbarProvider } from 'notistack';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <SnackbarProvider maxSnack={3}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Story />
+        </BrowserRouter>
+      </ThemeProvider>
+    </SnackbarProvider>
+  ),
+];
