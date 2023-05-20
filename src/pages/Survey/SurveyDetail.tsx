ad86bae737ad53ac75c82eed7b26c005f86ffa87
김지형 <jihyoung9912@gmail.com>
2023-05-21 01:39:32 +0900

    feat: add Navigate to click Callback effect;

--- src/pages/Survey/SurveyDetail.tsx
+++ src/pages/Survey/SurveyDetail.tsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { bannerState } from 'states/stateBanner';
 
 const SurveyDetail = () => {
+  const navigate = useNavigate();
   const [bannerDataState, setBannerDataState] = useRecoilState(bannerState);
   const [temp, setTemp] = useState(false);
 
   if (!temp) {
     setBannerDataState({
-      title: 'asd',
-      content: 'asd',
+      title: '로그인 후 기프티콘 결과를 확인하세요!',
+      content: '로그인',
+      callback: () => navigate('/login'),
     });
     setTemp(true);
   }
