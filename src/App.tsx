65ee84f32506f8c989181ad8603d5e3ee0af4b30
minjj0905 <minjj0905@daum.net>
2023-05-29 19:17:35 +0900

    💄 add: mui theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { theme, GlobalStyles } from 'styles';
+import { muiTheme, styledTheme, GlobalStyles } from 'styles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from 'layouts/Layout';
 import {
@@ -21,40 +21,42 @@ import {
 import { Modal } from 'components/common';
 import { RecoilRoot } from 'recoil';
 import { SnackbarProvider } from 'notistack';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material';
 import 'styles/fonts.css';
 
 const App = () => {
   return (
     <SnackbarProvider maxSnack={3}>
-      <ThemeProvider theme={theme}>
-        <RecoilRoot>
-          <Modal />
-          <GlobalStyles />
-          <Router>
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/mypage" element={<MyPage />} />
-                <Route path="/tos" element={<Tos />} />
-                <Route path="/privacy" element={<Privacy />} />
-                <Route path="*" element={<NotFound />} />
-
-                <Route path="/survey" element={<Survey />} />
-                <Route path="/survey/search" element={<SurveySearch />} />
-                <Route path="/survey/:id" element={<SurveyDetail />} />
-                <Route path="/survey/:id/result" element={<SurveyResult />} />
-                <Route path="/survey/:id/gift-result" element={<SurveyGiftResult />} />
-                <Route path="/survey/:id/submit" element={<SurveySubmit />} />
-                <Route path="/survey/new" element={<NewSurvey />} />
-                <Route path="/survey/new/form" element={<NewSurveyForm />} />
-                <Route path="/survey/new/gift" element={<NewSurveyGift />} />
-                <Route path="/survey/new/gift/gifticons" element={<NewSurveyGifticons />} />
-              </Route>
-            </Routes>
-          </Router>
-        </RecoilRoot>
-      </ThemeProvider>
+      <MuiThemeProvider theme={muiTheme}>
+        <StyledThemeProvider theme={styledTheme}>
+          <RecoilRoot>
+            <Modal />
+            <GlobalStyles />
+            <Router>
+              <Routes>
+                <Route element={<Layout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/mypage" element={<MyPage />} />
+                  <Route path="/tos" element={<Tos />} />
+                  <Route path="/privacy" element={<Privacy />} />
+                  <Route path="/survey" element={<Survey />} />
+                  <Route path="/survey/search" element={<SurveySearch />} />
+                  <Route path="/survey/:id" element={<SurveyDetail />} />
+                  <Route path="/survey/:id/result" element={<SurveyResult />} />
+                  <Route path="/survey/:id/gift-result" element={<SurveyGiftResult />} />
+                  <Route path="/survey/:id/submit" element={<SurveySubmit />} />
+                  <Route path="/survey/new" element={<NewSurvey />} />
+                  <Route path="/survey/new/form" element={<NewSurveyForm />} />
+                  <Route path="/survey/new/gift" element={<NewSurveyGift />} />
+                  <Route path="/survey/new/gift/gifticons" element={<NewSurveyGifticons />} />
+                  <Route path="*" element={<NotFound />} />
+                </Route>
+              </Routes>
+            </Router>
+          </RecoilRoot>
+        </StyledThemeProvider>
+      </MuiThemeProvider>
     </SnackbarProvider>
   );
 };
