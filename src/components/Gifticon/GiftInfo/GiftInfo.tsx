319984753126d07b9fbc6949e3ae276f51d07a49
soyekwon <soye0710@naver.com>
2023-06-07 14:48:54 +0900

    style: 기프티콘 전달 확인페이지 반응형 적용

--- src/components/Gifticon/GiftInfo/GiftInfo.tsx
+++ src/components/Gifticon/GiftInfo/GiftInfo.tsx
@@ -1,4 +1,5 @@
-import { Flex, Label } from 'components/Box';
+import { Flex, Box, Label } from 'components/Box';
+import { COLORS } from 'constants/COLOR';
 import giftType from './types';
 
 interface Props {
@@ -8,38 +9,43 @@ interface Props {
 const GiftInfo = (props: Props) => {
   const { gift } = props;
   return (
-    <Flex justifyContent="space-between" m="1rem" width="76.6%">
-      <Flex>
-        <img
-          src={`${process.env.PUBLIC_URL}/assets/images/starbucks.jpg`}
-          alt="IBELEVESURVEY Logo"
-          style={{ width: '6.75rem', borderRadius: '1rem' }}
-        />
-        <Flex flexDirection="column" gap="1.5rem" p="1rem">
-          <Label fontFamily="Pr-SemiBold" fontSize="0.9rem">
-            {gift.title}
-          </Label>
-          <Flex gap="1rem">
-            <Label fontFamily="Pr-Regular" fontSize="0.9rem">
-              {gift.price}원
+    <>
+      <Flex m="1rem" width="85%" justifyContent="space-between" cursor="pointer">
+        <Flex>
+          <img
+            src={`${process.env.PUBLIC_URL}/assets/images/starbucks.jpg`}
+            alt="IBELEVESURVEY Logo"
+            style={{ width: '6rem', borderRadius: '1.2rem' }}
+          />
+          <Flex flexDirection="column" gap="1.5rem" p="1rem" width="100%">
+            <Label fontFamily="Pr-SemiBold" fontSize="0.85rem" width="11.4rem">
+              {gift.title}
             </Label>
-            <Label fontFamily="Pr-Regular" fontSize="0.9rem">
-              {gift.cnt}개
-            </Label>
+            <Flex gap="1rem">
+              <Label fontFamily="Pr-Regular" fontSize="0.85rem">
+                {gift.price}원
+              </Label>
+              <Label fontFamily="Pr-Regular" fontSize="0.85rem">
+                {gift.cnt}개
+              </Label>
+            </Flex>
           </Flex>
         </Flex>
+
+        <Label fontFamily="Pr-Regular" fontSize="0.9rem" mt="2.5rem">
+          {gift.giver}
+        </Label>
+        <Flex flexDirection="column" gap="0.5rem" mt="1.4rem">
+          {gift.taker.map((data: string) => (
+            <Label fontFamily="Pr-Regular" fontSize="0.9rem" key={data}>
+              {data}
+            </Label>
+          ))}
+        </Flex>
       </Flex>
-      <Label fontFamily="Pr-Regular" fontSize="0.9rem" mt="4%">
-        {gift.giver}
-      </Label>
-      <Flex flexDirection="column" gap="0.5rem" ml="4%" mt="1%">
-        {gift.taker.map((data: string) => (
-          <Label fontFamily="Pr-Regular" fontSize="0.9rem" key={data}>
-            {data}
-          </Label>
-        ))}
-      </Flex>
-    </Flex>
+
+      <Box width="100%" height="0.1rem" background={COLORS.primaryVariant} />
+    </>
   );
 };
 
--- src/components/Gifticon/GiftList/GiftList.tsx
+++ src/components/Gifticon/GiftList/GiftList.tsx
@@ -1,12 +1,13 @@
 import { Flex, Box, Label } from 'components/Box';
 import { GiftInfo } from 'components/Gifticon';
 import { COLORS } from 'constants/COLOR';
+import Pagination from '@mui/material/Pagination';
 import dummyGifts from '../GiftInfo/dummyGifts';
 
 const GiftList = () => {
   return (
-    <Flex flexDirection="column">
-      <Box width="90%" height="2.25rem" background={COLORS.primaryVariant} p={2}>
+    <Flex flexDirection="column" alignItems="center">
+      <Box width="89%" height="2.25rem" background={COLORS.primaryVariant} p={2}>
         <Flex justifyContent="space-around">
           <Label fontSize="0.95rem" fontFamily="Pr-Bold" mr="3rem">
             전달한 기프티콘 목록
@@ -19,8 +20,14 @@ const GiftList = () => {
           </Label>
         </Flex>
       </Box>
-      <Box width="90%" height="2px" background={COLORS.primary} />
-      <GiftInfo gift={dummyGifts} />
+      <Box width="89%" height="2px" background={COLORS.primary} />
+      <Flex flexDirection="column" width="89%" mb="2rem">
+        <GiftInfo gift={dummyGifts} />
+        <GiftInfo gift={dummyGifts} />
+        <GiftInfo gift={dummyGifts} />
+        <GiftInfo gift={dummyGifts} />
+      </Flex>
+      <Pagination />
     </Flex>
   );
 };
