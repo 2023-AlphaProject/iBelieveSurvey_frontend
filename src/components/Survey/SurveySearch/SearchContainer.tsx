fc39ce94c3255a439179a9c5e2418471c1e27954
김지형 <jihyoung9912@gmail.com>
2023-06-13 11:41:50 +0900

    feat: Container Layout

--- src/components/Survey/SurveySearch/SearchContainer.tsx
+++ src/components/Survey/SurveySearch/SearchContainer.tsx
@@ -1,10 +1,31 @@
+import { Flex } from 'components/Box';
+import styled from 'styled-components';
+import { COLORS } from 'constants/COLOR';
+
+const Container = styled(Flex)`
+  flex-wrap: wrap;
+  width: 100%;
+  height: 12rem;
+  border: 3px solid ${COLORS.secondary};
+  border-radius: 25px;
+`;
+
+const InnerContainer = styled(Flex)`
+  width: 100%;
+  border-bottom: 2px solid ${COLORS.secondary};
+  :nth-child(4) {
+    border: none;
+  }
+`;
+
 const SearchContainer = () => {
   return (
-    <div>
-      asd
-      <div>asd</div>
-      <div>ads</div>
-    </div>
+    <Container>
+      <InnerContainer>ads</InnerContainer>
+      <InnerContainer>ads</InnerContainer>
+      <InnerContainer>ads</InnerContainer>
+      <InnerContainer>ads</InnerContainer>
+    </Container>
   );
 };
 
